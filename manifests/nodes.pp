a843b46127f230a4ae695d8abb3b34ac16df7ab2
Mikael Allison <mikael.allison@digital.landregistry.gov.uk>
2015-03-25 22:08:57 +0000

    applying puppet apply scripts

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,6 +1,22 @@
 
 
-node 'puppetmaster', 'ip-172-31-39-36' {
+node 'puppetmaster' {
+
+  package {'nmap':
+    ensure  =>  installed,
+  }
+
+  file {'/usr/local/bin/papply': 
+    mode    =>  '0755',
+    source  =>  "puppet:///modules/puppet/papplyu.sh",
+
+  }
+
+}
+
+node 'ip-172-31-39-36' {
+  include puppet
+
   package {'nmap':
     ensure  =>  installed,
   }
